0967c254902f9abbbba5a50720dcfebba1cc0cc1
chuseunghee <grittychu@gmail.com>
2022-11-11 14:42:02 +0900

    [추승희]레이아웃디자인진행중

---
+++ wantoffice/src/components/common/Header.js
@@ -0,0 +1,17 @@
+
+import HeaderCSS from "./Header.module.css";
+
+
+function Header() {
+
+    return (
+        <>
+            <div className={ HeaderCSS.HeaderDiv }>
+            <h1>헤더</h1> 
+            </div>
+        </>
+    );
+
+}
+
+export default Header;
