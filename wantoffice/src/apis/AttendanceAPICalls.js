137ec22f83a1ed64a5dd4abca8c7e6c53ad8dc91
김현진 <jinnni919191@gmail.com>
2022-11-23 16:41:52 +0900

    [김현진] 출퇴근 병합

--- wantoffice/src/apis/AttendanceAPICalls.js
+++ wantoffice/src/apis/AttendanceAPICalls.js
@@ -1,4 +1,4 @@
-import { POST_IN, GET_IN, POST_OUT, GET_OUT } from "../modules/AttendanceModule";
+import { POST_IN, GET_IN_OUT, POST_OUT } from "../modules/AttendanceModule";
 
 export const callInRegistAPI = () => {
 
@@ -24,9 +24,9 @@ export const callInRegistAPI = () => {
 
 }
 
-export const callInAPI = () => {
+export const callInOutAPI = () => {
 
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/api/attendance/in`;
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/api/attendance/inout`;
 
     return async (dispatch, getState) => {
 
@@ -42,7 +42,7 @@ export const callInAPI = () => {
 
         if(result.status === 200) {
             console.log('[AttendanceAPICalls] callInAPI result : ', result);
-            dispatch({ type: GET_IN, payload: result.data });
+            dispatch({ type: GET_IN_OUT, payload: result.data });
         }
 
     }
@@ -67,31 +67,6 @@ export const callOutRegistAPI = () => {
         if(result.status === 200) {
             console.log('[AttendanceAPICalls] callOutRegistAPI result : ', result);
             dispatch({ type: POST_OUT, payload: result.data });
-        }
-
-    }
-
-}
-
-export const callOutAPI = () => {
-
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/api/attendance/out`;
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(requestURL, {
-            method : "GET",
-            headers : {
-                "Content-Type" : "application/json",
-                "Accept" : "*/*",
-                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
-            }
-        })
-        .then(response => response.json());
-
-        if(result.status === 200) {
-            console.log('[AttendanceAPICalls] callOutAPI result : ', result);
-            dispatch({ type: GET_OUT, payload: result.data });
         }
 
     }
--- wantoffice/src/modules/AttendanceModule.js
+++ wantoffice/src/modules/AttendanceModule.js
@@ -5,17 +5,15 @@ const initialState = [];
 
 /* 액션 */
 export const POST_IN = 'attendance/POST_IN';
-export const GET_IN = 'attendance/GET_IN';
+export const GET_IN_OUT = 'attendance/GET_IN_OUT';
 export const POST_OUT = 'attendance/POST_OUT';
-export const GET_OUT = 'attendance/GET_OUT';
 export const GET_MY = 'attendance/GET_MY';
 export const GET_MANAGE_LIST = 'attendance/GET_MANAGE_LIST';
 
 const actions = createActions({
     [POST_IN]: () => {},
-    [GET_IN]: () => {},
+    [GET_IN_OUT]: () => {},
     [POST_OUT]: () => {},
-    [GET_OUT]: () => {},
     [GET_MY]: () => {},
     [GET_MANAGE_LIST]: () => {}
 });
@@ -26,13 +24,10 @@ const attendanceReducer = handleActions(
         [POST_IN] : (state, { payload }) => {
             return payload;
         },
-        [GET_IN] : (state, { payload }) => {
+        [GET_IN_OUT] : (state, { payload }) => {
             return payload;
         },
         [POST_OUT] : (state, { payload }) => {
-            return payload;
-        },
-        [GET_OUT] : (state, { payload }) => {
             return payload;
         },
         [GET_MY] : (state, { payload }) => {
