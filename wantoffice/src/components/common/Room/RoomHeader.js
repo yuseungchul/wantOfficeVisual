1a84642c0acdfe0e1104ef617949cfb808dafec1
Kimhg <dhtpf1859@gmail.com>
2022-11-24 09:41:55 +0900

    [김형기] npm start 문제 해결

--- wantoffice/src/components/common/Room/RoomHeader.js
+++ wantoffice/src/components/common/Room/RoomHeader.js
@@ -22,7 +22,7 @@ function Header() {
 
                 <p>회의실 예약 조회</p>
                 <ul>
-                    {/* <li><NavLink to="room/rvlist">&#9654; 회의실 예약 조회</NavLink></li> */}
+                    <li><NavLink to="/">&#9654; 회의실 예약 조회</NavLink></li>
                     <li><NavLink to="/">&#9654; 회의실 예약 등록</NavLink></li>
                     <li><NavLink to="/">&#9654; 회의실 예약 수정</NavLink></li>
                 </ul>
--- wantoffice/src/modules/reservationModule.js
+++ wantoffice/src/modules/reservationModule.js
@@ -1,28 +1,28 @@
-import { createActions, handleActions } from "redux-actions";
+// import { createActions, handleActions } from "redux-actions";
 
-/* 초기값 */
-const initialState = [];
+// /* 초기값 */
+// const initialState = [];
 
-/* 액션 */
-export const GET_ROOM = 'room/GET_ROOM';
-export const GET_ROOMS = 'room/GET_ROOMS';
+// /* 액션 */
+// export const GET_ROOM = 'room/GET_ROOM';
+// export const GET_ROOMS = 'room/GET_ROOMS';
 
-const actions = createActions({
-    [GET_ROOM]: () => {},
-    [GET_ROOMS]: () => {}
-});
+// const actions = createActions({
+//     [GET_ROOM]: () => {},
+//     [GET_ROOMS]: () => {}
+// });
 
-/* 리듀서 */
-const reservationReducer = handleActions(
-    {
-        [GET_ROOM] : (state, { payload }) =>{
-            return payload;
-        },
-        [GET_ROOMS] : (state, { payload }) =>{
-            return payload;
-        }
-    },
-    initialState
-);
+// /* 리듀서 */
+// const reservationReducer = handleActions(
+//     {
+//         [GET_ROOM] : (state, { payload }) =>{
+//             return payload;
+//         },
+//         [GET_ROOMS] : (state, { payload }) =>{
+//             return payload;
+//         }
+//     },
+//     initialState
+// );
 
-export default reservationReducer;
+// export default reservationReducer;
