6e671892bc5154cbee15f17f632414f4fa831b3b
SU JIN KIM <rosysnown@gmail.com>
2022-12-01 18:50:22 +0900

    [김수진] 사원 정보 수정 관련 코드 작업 중

--- wantoffice/src/modules/MemberModules.js
+++ wantoffice/src/modules/MemberModules.js
@@ -2,19 +2,34 @@ import { createActions, handleActions } from "redux-actions";
 
 const initialState = [];
 
+export const GET_MEMBERS = 'member/GET_MEMBERS';
 export const GET_MEMBER = 'member/GET_MEMBER';
+export const PUT_MEMBER = 'member/PUT_MEMBER';
+export const DELETE_MEMBER = 'member/DELETE_MEMBER';
 export const POST_LOGIN = 'member/POST_LOGIN';
 export const POST_REGISTER = 'member/POST_REGISTER';
 
 const actions = createActions({
+    [GET_MEMBERS]: () => {},
     [GET_MEMBER]: () => {},
+    [PUT_MEMBER]: () => {},
+    [DELETE_MEMBER]: () => {},
     [POST_LOGIN]: () => {},
     [POST_REGISTER]: () => {},
 });
 
 const memberReducer = handleActions(
     {
+        [GET_MEMBERS] : (state, { payload }) => {
+            return payload;
+        },
         [GET_MEMBER] : (state, { payload }) => {
+            return payload;
+        },
+        [PUT_MEMBER] : (state, { payload }) => {
+            return payload;
+        },
+        [DELETE_MEMBER] : (state, { payload }) => {
             return payload;
         },
         [POST_LOGIN] : (state, { payload }) => {
